188459456c578d78b99bf853e99a6da8ad644598
weoamathior <weoamathior@gmail.com>
2024-01-25 19:30:35 -0800

    docker compose and init data

--- application/job-service/src/main/java/irish/bla/jobservice/controller/JobController.java
+++ application/job-service/src/main/java/irish/bla/jobservice/controller/JobController.java
@@ -17,16 +17,19 @@ import java.util.Set;
 public class JobController {
     private final JobService jobService;
 
+    //curl localhost:8080/job/all
     @GetMapping("all")
     public Flux<JobDto> all() {
         return jobService.allJobs();
     }
 
+    //curl 'localhost:8080/job/search?skills=docker'
     @GetMapping("search")
     public Flux<JobDto> search(@RequestParam Set<String> skills) {
         return jobService.jobsBySkills(skills);
     }
 
+    // curl -XPOST -H 'Content-type:application/json' -d '{"description":"head guy", "salary": 10, "remote":true, "company":"apppl","skills":["eating","sleeping"]}' 'localhost:8080/job'
     @PostMapping
     public Mono<JobDto> save(@RequestBody Mono<JobDto> mono) {
         return jobService.save(mono);
