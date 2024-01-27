9fbcf9c0af3a541516a23f6ff834a82f7440d29c
weoamathior <weoamathior@gmail.com>
2024-01-27 10:46:06 -0800

    setting up integration test with mockserver config

--- application/candidate-service/src/test/java/irish/bla/candidateservice/BaseTest.java
+++ application/candidate-service/src/test/java/irish/bla/candidateservice/BaseTest.java
@@ -1,5 +1,6 @@
 package irish.bla.candidateservice;
 
+import irish.bla.candidateservice.dto.Service;
 import org.junit.ClassRule;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -15,9 +16,20 @@ import java.io.File;
 
 @Testcontainers
 public abstract class BaseTest {
-    public static final int MONGO_PORT = 27017;
-    public static final String MONGO = "mongo";
-    public static final String MONGO_URI_FORMAT = "mongodb://candidate_user:candidate_password@%s:%s/candidate";
+    public static final Service MONGO = Service.create(
+            "mongo",
+            27017,
+            "0",
+            "mongodb://candidate_user:candidate_password@%s:%s/candidate",
+            "HOST_PORT1"
+    )
+    public static final Service JOB = Service.create(
+            "job-mock",
+            1080,
+            "0",
+            "http://%s:%s/job/",
+            "HOST_PORT2"
+    )
 
     @ClassRule
     public static final DockerComposeContainer<?> compose = new DockerComposeContainer<>(new File("docker-compose.yaml"));
@@ -25,13 +37,20 @@ public abstract class BaseTest {
     @DynamicPropertySource
     static void mongoProperties(DynamicPropertyRegistry registry) {
         compose
-                .withEnv("HOST_ENV", "0") // "0" means assign a random port
-                .withExposedService(MONGO, MONGO_PORT, Wait.forListeningPort())
+                .withEnv(MONGO.getHostPortEnvVariable(), MONGO.getHostPort())
+                .withEnv(JOB.getHostPortEnvVariable(), JOB.getHostPort())
+                .withExposedService(MONGO.getName(), MONGO.getPort(), Wait.forListeningPort())
+                .withExposedService(JOB.getName(), JOB.getPort(), Wait.forListeningPort())
                 .start();
-        String serviceHost = compose.getServiceHost(MONGO, MONGO_PORT);
-        Integer servicePort = compose.getServicePort(MONGO, MONGO_PORT);
+        String mongoHost = compose.getServiceHost(MONGO.getName(), MONGO.getPort());
+        Integer mongoPort = compose.getServicePort(MONGO.getName(), MONGO.getPort());
 
-        String uri = String.format(MONGO_URI_FORMAT, serviceHost, servicePort);
+        String uri = String.format(MONGO.getUri(), mongoHost, mongoPort);
         registry.add("spring.data.mongodb.uri", () -> uri);
+
+        String jobHost = compose.getServiceHost(JOB.getName(), JOB.getPort());
+        Integer jobPort = compose.getServicePort(JOB.getName(), JOB.getPort());
+        String joburi = String.format(JOB.getUri(), jobHost, jobPort);
+        registry.add("job.service.url", () -> joburi);
     }
 }
---
+++ application/candidate-service/src/test/java/irish/bla/candidateservice/dto/Service.java
@@ -0,0 +1,14 @@
+package irish.bla.candidateservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor(staticName = "create")
+public class Service {
+    private String name;
+    private int port;
+    private String hostPort;
+    private String uri;
+    private String hostPortEnvVariable;
+}
