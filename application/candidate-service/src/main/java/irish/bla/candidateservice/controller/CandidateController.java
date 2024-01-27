891efe6689768f5419b3cdbb899953a112434721
weoamathior <weoamathior@gmail.com>
2024-01-27 07:40:17 -0800

    find candidate by id

--- application/candidate-service/src/main/java/irish/bla/candidateservice/controller/CandidateController.java
+++ application/candidate-service/src/main/java/irish/bla/candidateservice/controller/CandidateController.java
@@ -5,9 +5,11 @@ import irish.bla.candidateservice.service.CandidateService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @RestController
@@ -20,5 +22,10 @@ public class CandidateController {
     @GetMapping("all")
     public Flux<CandidateDto> all() {
         return candidateService.allCandidates();
+    }
+
+    @GetMapping("{candidateId}")
+    public Mono<CandidateDto> findById(@PathVariable String candidateId) {
+        return candidateService.findById(candidateId);
     }
 }
--- application/candidate-service/src/main/java/irish/bla/candidateservice/service/CandidateService.java
+++ application/candidate-service/src/main/java/irish/bla/candidateservice/service/CandidateService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @Service
@@ -17,6 +18,10 @@ public class CandidateService {
     public Flux<CandidateDto> allCandidates() {
         return candidateRepository.findAll()
                 .map(EntityDtoUtil::toDto);
+    }
+
+    public Mono<CandidateDto> findById(String id) {
+        return candidateRepository.findById(id).map(EntityDtoUtil::toDto);
     }
 
 }
