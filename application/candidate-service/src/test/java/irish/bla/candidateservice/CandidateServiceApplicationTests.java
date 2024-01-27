7e1dec1194ad967a1209c824668bfd86c2100b6c
weoamathior <weoamathior@gmail.com>
2024-01-27 08:10:33 -0800

    mock repository in test

--- application/candidate-service/src/test/java/irish/bla/candidateservice/CandidateServiceApplicationTests.java
+++ application/candidate-service/src/test/java/irish/bla/candidateservice/CandidateServiceApplicationTests.java
@@ -1,9 +1,17 @@
 package irish.bla.candidateservice;
 
+import irish.bla.candidateservice.repository.CandidateRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = MongoReactiveAutoConfiguration.class)
+@MockBean({
+	CandidateRepository.class
+})
 class CandidateServiceApplicationTests {
 
 	@Test
